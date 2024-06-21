3e3988f73a403255bb135d163c739cb1ee740268
paalwilliams-aaron-ai <158501128+paalwilliams-aaron-ai@users.noreply.github.com>
2024-06-21 13:47:18 +0200

    adjust init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
- stow nvim --target=$HOME/.config
- stow fish --target=$HOME/.config
- stow kitty --target=$HOME/.config
- stow karabiner --target=$HOME/.config
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+curl https://get.volta.sh | bash
+
+stow nvim --target=$HOME/.config
+stow fish --target=$HOME/.config
+stow kitty --target=$HOME/.config
+stow karabiner --target=$HOME/.config
