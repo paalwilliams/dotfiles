c775e638b844a6a49ad8c4319e11c5381d355af1
paul.williams <paul.williams@macm-dcqkhwdggk.fritz.box>
2024-06-21 15:05:10 +0200

    changes, remove dead code

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -6,16 +6,19 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 pyenv init - | source
 zoxide init fish | source
-export OPENAI_API_KEY=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
-
-export ANDROID_HOME="$HOME/Library/Android/Sdk"
-export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
-export ANDROID_AVD_HOME="./android"
-
-
-# Custom Aliases
+##
+#export ANDROID_HOME="$HOME/Library/Android/Sdk"
+#export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+#export ANDROID_AVD_HOME="./android"
+#
+#
+## Custom Aliases
 alias START_ANDROID_EMU="emulator -avd Pixel_3a_API_34_extension_level_7_arm64-v8a"
 alias LATEST_TAGS="git tag --sort=-creatordate | grep int | head -n 1 &&  git tag --sort=-creatordate | grep pre-prod | head -n 1 &&  git tag --sort=-creatordate | grep aaron-prod | head -n 1"
 alias local_dev="cd /Users/paalwilliams/src/aaron/product_core/local_dev && ./start.sh"
 alias cls="clear"
 alias icat="kitten icat"
+
+
+# rbenv
+status --is-interactive; and rbenv init - fish | source
--- init.sh
+++ init.sh
@@ -1,8 +1,46 @@
+# Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Node.JS
 curl https://get.volta.sh | bash
+volta install node
+volta install node@20
+volta install node@16
 
+# Stow and dotfiles
+brew install stow
+mkdir ~/.config
 stow nvim --target=$HOME/.config
 stow fish --target=$HOME/.config
 stow kitty --target=$HOME/.config
 stow karabiner --target=$HOME/.config
+
+# Nerd Font
+brew install --cask font-hack-nerd-font
+
+# Homebrew Packages 
+brew install rectangle
+brew install zoxide
+brew install neovim
+brew install kitty
+brew install visual-studio-code
+brew install fish
+brew install ripgrep
+brew install karabiner-elements
+brew install alfred
+brew install firefox
+
+# Pyenv
+brew install pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+pyenv install 3
+
+# rbenv
+brew install rbenv ruby-build
+rbenv init
+
+# Fish Setup
+sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+chsh -s /opt/homebrew/bin/fish
+
