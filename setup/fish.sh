578387f51041f1c7b20e5e5f695a63aa734f69d1
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 00:41:49 +0100

    add config.fish and fish_plugins

--- setup/fish.sh
+++ setup/fish.sh
@@ -2,3 +2,5 @@
 sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 chsh -s /opt/homebrew/bin/fish
 
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
