4f7002ff3446c98cbc651b2733291ba67df07543
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 00:12:09 +0100

    fix pyenv crap

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,20 +1,10 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-#
+
 zoxide init fish | source
-##
-#export ANDROID_HOME="$HOME/Library/Android/Sdk"
-#export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
-#export ANDROID_AVD_HOME="./android"
-#
-#
+
 ## Custom Aliases
-alias START_ANDROID_EMU="emulator -avd Pixel_3a_API_34_extension_level_7_arm64-v8a"
-alias LATEST_TAGS="git tag --sort=-creatordate | grep int | head -n 1 &&  git tag --sort=-creatordate | grep pre-prod | head -n 1 &&  git tag --sort=-creatordate | grep aaron-prod | head -n 1"
-alias local_dev="cd /Users/paul.williams/src/aaron/product_core/local_dev && ./start.sh"
+
 alias cls="clear"
 alias icat="kitten icat"
 
@@ -22,6 +12,5 @@ alias icat="kitten icat"
 # rbenv
 status --is-interactive; and rbenv init - fish | source
 
-#pyenv init - | source
-
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
