7d1895073fe09920d30bdd7976d6af9b59634a40
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 00:49:00 +0100

    more cleanup

--- setup/fish.sh
+++ setup/fish.sh
@@ -1,6 +1,6 @@
-# Fish Setup
 sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 chsh -s /opt/homebrew/bin/fish
 
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+# Install plugins from fish/fish_plugins
+fisher update
