fbb019cf1ff953c17a0288f4235240598066d3fc
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 01:58:21 +0100

    cleanup gitignore and stow.sh

--- setup/stow.sh
+++ setup/stow.sh
@@ -1,7 +1,10 @@
 # Stow - symlink dotfiles
 mkdir -p ~/.config
 stow nvim --target=$HOME/.config
+
+mkdir -p $HOME/.config/fish
 stow fish --target=$HOME/.config
+
 stow karabiner --target=$HOME/.config
 stow alacritty --target=$HOME/.config
  
