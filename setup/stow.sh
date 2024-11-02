1e1a4e32b2e65476efeabb663d0e13186640b250
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 02:23:41 +0100

    refactor stow command

--- setup/stow.sh
+++ setup/stow.sh
@@ -1,10 +1,4 @@
 # Stow - symlink dotfiles
 mkdir -p ~/.config
-stow nvim --target=$HOME/.config
-
 mkdir -p $HOME/.config/fish
-stow fish --target=$HOME/.config
-
-stow karabiner --target=$HOME/.config
-stow alacritty --target=$HOME/.config
- 
+stow nvim fish karabiner alacritty  --target=$HOME/.config
