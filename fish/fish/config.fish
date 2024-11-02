df2e1878f27e37f4dcc8fa6c8c0ca34f251f07d4
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 01:45:08 +0100

    cleanup

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,3 +1,14 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH 
+
+status --is-interactive; and source (rbenv init -|psub)
+
+# Custom Aliases
+
+alias cls="clear"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+zoxide init fish | source
+
+pyenv init - | source
--- setup/stow.sh
+++ setup/stow.sh
@@ -2,10 +2,6 @@
 mkdir -p ~/.config
 stow nvim --target=$HOME/.config
 stow fish --target=$HOME/.config
-ln -s ../fish/fish/config.fish $HOME/.config/fish/config.fish 
-ln -s ../fish/fish/fish_variables $HOME/.config/fish/fish_variables
-
-stow kitty --target=$HOME/.config
 stow karabiner --target=$HOME/.config
 stow alacritty --target=$HOME/.config
  
