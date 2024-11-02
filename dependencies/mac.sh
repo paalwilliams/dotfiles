f62fd953a065324f14adc0c58daec015136fbb3c
Paal <paal@Paals-MacBook-Pro-3.local>
2024-11-02 02:02:28 +0100

    add file arg to brew bundle install

--- dependencies/mac.sh
+++ dependencies/mac.sh
@@ -1,5 +1,5 @@
-# Install Homebrew
+# Install eomebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Homebrew Packages 
-brew bundle install 
+brew bundle install --file=./dependencies/Brewfile
